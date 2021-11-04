e56b876b16c0620c1db3cde5319fc03cf97e2791
LeonardoWelter <leonardo.dias_silva@hotmail.com>
2021-11-04 15:51:38 -0300

    * Change Project Information;

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'WP Bio'
+copyright = '2021, Leonardo Welter'
+author = 'Leonardo Welter'
 
 release = '0.1'
 version = '0.1.0'
